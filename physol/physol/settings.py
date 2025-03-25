b0623fc1fbe83b61f86756e2cf1615e50392d7fb
tanzhixin <tanzhxin@hotmail.com>
2025-03-25 08:09:59 +0800

    disable debug, and open to all ip    in settings.py.

--- physol/physol/settings.py
+++ physol/physol/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8v1ol+%f2)_u(s7#oc0x97i)nx#gdx^+6ey6*nj7m9cg$f%_ev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
