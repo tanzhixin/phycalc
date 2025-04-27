f7fa124835c1edad8fd72b7224b711f30155740e
tanzhixin <tanzhxin@hotmail.com>
2025-04-27 10:32:18 +0800

    add  urls.py   templates/paa.html   for proton analysis.

--- physol/calcol/urls.py
+++ physol/calcol/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('pcalc', views.pcalc, name='physics calculation'),
     path('mcalc', views.mcalc, name='mathmatics calculator'),
     path('ncalc', views.ncalc, name='neutron calculation'),
+    path('paa', views.paa, name='proton activation analysis'),
     path('pwg', views.pwg, name='password generator'), 
     path('',views.index, name='index')
 ]
